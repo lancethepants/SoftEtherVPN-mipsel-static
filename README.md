76469c7e002293ac1f3e6c24b07a39b3a4613d37
lancethepants <lancethepants@gmail.com>
2014-03-31 09:57:20 -0600

    Add SoftEtherVPN.sh build script.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 SoftEtherVPN-mipsel-static
 ==========================
+
+Script to create static SoftEtherVPN binaries for mipsel devices using the entware toolchain.
+
+Make sure the entware toolchain is already in your PATH, and run.
+
+The resulting binary is available at http://lancethepants.com/files
---
+++ SoftEtherVPN.sh
@@ -0,0 +1,173 @@
+ #!/bin/bash
+
+set -e
+set -x
+
+mkdir ~/softether && cd ~/softether
+
+BASE=`pwd`
+SRC=$BASE/src
+WGET="wget --prefer-family=IPv4"
+RPATH=/opt/lib
+DEST=$BASE/opt
+LDFLAGS="-L$DEST/lib -Wl,--gc-sections"
+CPPFLAGS="-I$DEST/include -I$DEST/include/ncursesw"
+CFLAGS="-mtune=mips32 -mips32 -O3 -ffunction-sections -fdata-sections"
+CXXFLAGS=$CFLAGS
+CONFIGURE="./configure --prefix=/opt --host=mipsel-linux"
+MAKE="make -j`nproc`"
+mkdir $SRC
+
+######## ####################################################################
+# ZLIB # ####################################################################
+######## ####################################################################
+
+mkdir $SRC/zlib && cd $SRC/zlib
+$WGET http://zlib.net/zlib-1.2.8.tar.gz
+tar zxvf zlib-1.2.8.tar.gz
+cd zlib-1.2.8
+
+LDFLAGS=$LDFLAGS \
+CPPFLAGS=$CPPFLAGS \
+CFLAGS=$CFLAGS \
+CROSS_PREFIX=mipsel-linux- \
+./configure \
+--prefix=/opt \
+--static
+
+$MAKE
+make install DESTDIR=$BASE
+
+########### #################################################################
+# OPENSSL # #################################################################
+########### #################################################################
+
+mkdir -p $SRC/openssl && cd $SRC/openssl
+$WGET http://www.openssl.org/source/openssl-1.0.1f.tar.gz
+tar zxvf openssl-1.0.1f.tar.gz
+cd openssl-1.0.1f
+
+cat << "EOF" > openssl.patch
+--- Configure_orig      2013-11-19 11:32:38.755265691 -0700
++++ Configure   2013-11-19 11:31:49.749650839 -0700
+@@ -402,6 +402,7 @@ my %table=(
+ "linux-alpha+bwx-gcc","gcc:-O3 -DL_ENDIAN -DTERMIO::-D_REENTRANT::-ldl:SIXTY_FOUR_BIT_LONG RC4_CHAR RC4_CHUNK DES_RISC1 DES_UNROLL:${alpha_asm}:dlfcn:linux-shared:-fPIC::.so.\$(SHLIB_MAJOR).\$(SHLIB_MINOR)",
+ "linux-alpha-ccc","ccc:-fast -readonly_strings -DL_ENDIAN -DTERMIO::-D_REENTRANT:::SIXTY_FOUR_BIT_LONG RC4_CHUNK DES_INT DES_PTR DES_RISC1 DES_UNROLL:${alpha_asm}",
+ "linux-alpha+bwx-ccc","ccc:-fast -readonly_strings -DL_ENDIAN -DTERMIO::-D_REENTRANT:::SIXTY_FOUR_BIT_LONG RC4_CHAR RC4_CHUNK DES_INT DES_PTR DES_RISC1 DES_UNROLL:${alpha_asm}",
++"linux-mipsel", "gcc:-DL_ENDIAN -DTERMIO -O3 -mtune=mips32 -mips32 -fomit-frame-pointer -Wall::-D_REENTRANT::-ldl:BN_LLONG RC4_CHAR RC4_CHUNK DES_INT DES_UNROLL BF_PTR:${mips32_asm}:o32:dlfcn:linux-shared:-fPIC::.so.\$(SHLIB_MAJOR).\$(SHLIB_MINOR)",
+
+ # Android: linux-* but without -DTERMIO and pointers to headers and libs.
+ "android","gcc:-mandroid -I\$(ANDROID_DEV)/include -B\$(ANDROID_DEV)/lib -O3 -fomit-frame-pointer -Wall::-D_REENTRANT::-ldl:BN_LLONG RC4_CHAR RC4_CHUNK DES_INT DES_UNROLL BF_PTR:${no_asm}:dlfcn:linux-shared:-fPIC::.so.\$(SHLIB_MAJOR).\$(SHLIB_MINOR)",
+EOF
+
+patch < openssl.patch
+
+./Configure linux-mipsel \
+-ffunction-sections -fdata-sections  -Wl,--gc-sections \
+--prefix=/opts zlib \
+--with-zlib-lib=$DEST/lib \
+--with-zlib-include=$DEST/include
+
+make CC=mipsel-linux-gcc
+make CC=mipsel-linux-gcc install INSTALLTOP=$DEST OPENSSLDIR=$DEST/ssl
+
+########### #################################################################
+# NCURSES # #################################################################
+########### #################################################################
+
+mkdir $SRC/curses && cd $SRC/curses
+$WGET http://ftp.gnu.org/pub/gnu/ncurses/ncurses-5.9.tar.gz
+tar zxvf ncurses-5.9.tar.gz
+cd ncurses-5.9
+
+LDFLAGS=$LDFLAGS \
+CPPFLAGS=$CPPFLAGS \
+CFLAGS=$CFLAGS \
+$CONFIGURE \
+--enable-widec \
+--with-shared \
+--disable-database \
+--with-fallbacks=xterm
+
+$MAKE
+make install DESTDIR=$BASE
+
+############### #############################################################
+# LIBREADLINE # #############################################################
+############### #############################################################
+
+mkdir $SRC/libreadline && cd $SRC/libreadline
+$WGET ftp://ftp.cwru.edu/pub/bash/readline-6.2.tar.gz
+tar zxvf readline-6.2.tar.gz
+cd readline-6.2
+
+$WGET https://raw.github.com/lancethepants/tomatoware/master/patches/readline.patch
+patch < readline.patch
+
+LDFLAGS=$LDFLAGS \
+CPPFLAGS=$CPPFLAGS \
+CFLAGS=$CFLAGS \
+$CONFIGURE \
+--disable-shared
+
+$MAKE
+make install DESTDIR=$BASE
+
+############ ################################################################
+# LIBICONV # ################################################################
+############ ################################################################
+
+mkdir $SRC/libiconv && cd $SRC/libiconv
+$WGET http://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.14.tar.gz
+tar zxvf libiconv-1.14.tar.gz
+cd libiconv-1.14
+
+LDFLAGS=$LDFLAGS \
+CPPFLAGS=$CPPFLAGS \
+CFLAGS=$CFLAGS \
+$CONFIGURE \
+--enable-static \
+--disable-shared
+
+$MAKE
+make install DESTDIR=$BASE
+
+############# ###############################################################
+# SOFTETHER # ###############################################################
+############# ###############################################################
+
+mkdir $SRC/softether && cd $SRC/softether
+git clone https://github.com/SoftEtherVPN/SoftEtherVPN.git
+cp -r SoftEtherVPN SoftEtherVPN_host
+cd SoftEtherVPN_host
+
+if [ "`uname -m`" == "x86_64" ];then
+	cp ./src/makefiles/linux_64bit.mak ./Makefile
+else
+	cp ./src/makefiles/linux_32bit.mak ./Makefile
+fi
+
+$MAKE
+
+cd ../SoftEtherVPN
+
+$WGET https://raw.github.com/el1n/OpenWRT-package-softether/master/patches/100-fix-ccldflags-common.patch
+$WGET https://raw.github.com/el1n/OpenWRT-package-softether/master/patches/120-fix-iconv-headers-common.patch
+patch -p1 < 100-fix-ccldflags-common.patch
+patch -p1 < 120-fix-iconv-headers-common.patch
+
+cp ./src/makefiles/linux_32bit.mak ./Makefile
+sed -i 's,CC=gcc,CC=mipsel-linux-gcc,g' Makefile
+sed -i 's,-lncurses -lz,-lncursesw -lz -liconv,g' Makefile
+sed -i 's,ranlib,mipsel-linux-ranlib,g' Makefile
+
+CCFLAGS="$CPPFLAGS $CFLAGS" \
+LDFLAGS="-static $LDFLAGS" \
+$MAKE \
+|| true
+
+cp ../SoftEtherVPN_host/tmp/hamcorebuilder ./tmp/
+
+CCFLAGS="$CPPFLAGS $CFLAGS" \
+LDFLAGS="-static $LDFLAGS" \
+$MAKE
